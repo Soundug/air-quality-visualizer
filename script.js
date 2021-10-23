b83cef0fe5782134e3ad10c339633450528d25f6
Soundarya G <soundaryagsk@gmail.com>
2021-10-23 16:29:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,381 @@
+
+// Populate the state dropdown from our JSON, the id of each state will be the FIPS code (used in the API for data retrieval)
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetch('states.json')
+    .then((res) => res.text())
+    .then((data) => { 
+        data = JSON.parse(data);
+        let output = '';
+        
+        // Create a button for each state
+        data.forEach(function(states) {
+            output += `<option class="dropdown-item selected_state" id="${states.state_code}">${states.state_name}</option>\r\n`;            
+        });
+        document.getElementById('select_state').innerHTML += output;
+
+    }) 
+    .catch((err) => console.log(err));
+})
+
+
+// If the user has selected a state, populate the counties for that state (the id for the counties are the FIPS codes)
+
+document.getElementById("select_state").onchange = populateCountyDropdown;
+
+function populateCountyDropdown() {
+
+    // Clear the dropdown list
+    document.getElementById("select_county").options.length=0;
+
+    var selected_state = document.getElementById('select_state');
+    var value = selected_state.options[selected_state.selectedIndex].id;
+
+    fetch('state_county_data.json')
+    .then((res) => res.text())
+    .then((data) => { 
+        data = JSON.parse(data);
+        let output = '';
+        // Create a button for each county in selected state
+        data.forEach(function(state_county_data) {
+            if (value == state_county_data.state_code) {
+                output += `<option class="dropdown-item" id="${state_county_data.county_code}">${state_county_data.county_name}</option>\r\n`;   
+            }         
+        });
+        document.getElementById('select_county').innerHTML += output;
+    }) 
+    .catch((err) => console.log(err));
+}
+
+// Call populateDateDropdowns when the year is clicked on
+document.getElementById("select_year").addEventListener("mousedown", populateDateDropdowns);
+
+// Fill the dropdowns with all possible years and months
+function populateDateDropdowns() {
+
+    for(let i = 0; i <= 40; i++) {
+
+        var year = 1980 + i;
+
+        document.getElementById('select_year').innerHTML += `<option class="dropdown-item">${year.toString()}</option>\r\n`;
+    }
+
+    for(let i = 0; i < 12; i++) {
+
+        var month = i + 1;
+
+        if(month < 10) {
+
+            document.getElementById('select_month').innerHTML += `<option class="dropdown-item">0${month.toString()}</option>\r\n`;
+        }
+        else {
+
+            document.getElementById('select_month').innerHTML += `<option class="dropdown-item">${month.toString()}</option>\r\n`;
+        }
+    }
+
+    document.getElementById("select_year").removeEventListener("mousedown", populateDateDropdowns);   
+}
+
+document.getElementById("select_month").onchange = populateDayDropdown;
+
+function populateDayDropdown() {
+
+    // Clear the dropdown list
+    document.getElementById("select_day").options.length=0;
+
+    var selected_month = document.getElementById('select_month');
+    var month = selected_month.options[selected_month.selectedIndex].text;
+
+    if(month == "2") {
+
+        for(let i = 0; i < 28; i++) {
+
+            var day = i + 1;
+    
+            if(day < 10) {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">0${day.toString()}</option>\r\n`;
+            }
+            else {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">${day.toString()}</option>\r\n`;
+            }
+        }
+    }
+    else if (month == "4" || month == "6" || month == "9" || month == "11") {
+
+        for(let i = 0; i < 30; i++) {
+
+            var day = i + 1;
+    
+            if(day < 10) {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">0${day.toString()}</option>\r\n`;
+            }
+            else {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">${day.toString()}</option>\r\n`;
+            }        }
+    }
+    else {
+        
+        for(let i = 0; i < 31; i++) {
+
+            var day = i + 1;
+    
+            if(day < 10) {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">0${day.toString()}</option>\r\n`;
+            }
+            else {
+
+                document.getElementById('select_day').innerHTML += `<option class="dropdown-item">${day.toString()}</option>\r\n`;
+            }        }
+    }
+}
+
+// Fetch states.json data
+function fetchStates() {
+
+    var jsondata;
+
+    return fetch('states.json')
+        .then(function(response) {
+            return response.json();
+    })  
+        .then(function(json) {
+            return json;
+    });
+}
+
+// Fetch state.county_data.json data
+function fetchState_County_Data() {
+
+    var jsondata;
+
+    return fetch('state_county_data.json')
+        .then(function(response) {
+            return response.json();
+    })  
+        .then(function(json) {
+            return json;
+    });
+}
+
+document.getElementById("select_day").onchange = getAPIData;
+
+function getAPIData() {
+
+    // Get selected state and county divs
+    var selected_state = document.getElementById('select_state');
+    var selected_county = document.getElementById('select_county');
+    var selected_year = document.getElementById('select_year');
+    var selected_month = document.getElementById('select_month');
+    var selected_day = document.getElementById('select_day');
+    // Get the value from above
+    var state = selected_state.options[selected_state.selectedIndex].text;
+    var county = selected_county.options[selected_county.selectedIndex].text;
+    var year = selected_year.options[selected_year.selectedIndex].text;
+    var month = selected_month.options[selected_month.selectedIndex].text;
+    var day = selected_day.options[selected_day.selectedIndex].text;
+
+    var state_code;
+    // Base url that needs to be added onto with state and county
+    var url = "https://aqs.epa.gov/data/api/dailyData/byCounty?email=rachel.peterson.5683@gmail.com&key=tealheron74&param=44201,88101";
+
+    url += "&bdate=" + year + month + day + "&edate=" + year + month + day;
+
+    // Call function to get states data and compare to dropdown state
+    fetchStates().then(function(data) {
+        data.forEach(function(states) {
+            if(state == states.state_name) {
+                state_code = states.state_code;
+                url += "&state=" + states.state_code;
+            }         
+        });
+    });
+
+    // Call function to get state_county_data and compare to dropdown county, also finally grab data with base url 
+    fetchState_County_Data().then(function(data) {
+        data.forEach(function(state_county_data) {
+            if(county == state_county_data.county_name && state_code == state_county_data.state_code) {
+                url += "&county=" + state_county_data.county_code;
+                console.log(county);
+                var xmlhttp = new XMLHttpRequest();
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => console.log(data));
+                    xmlhttp.open("GET", url, true);
+                    xmlhttp.send();
+                    xmlhttp.onreadystatechange = function(){
+                    if (this.readyState == 4 && this.status == 200){
+                        var data = JSON.parse(this.responseText);
+                        var st = data.Data.map(function(elem){
+                    if (elem.pollutant_standard == "PM25 24-hour 2006" || elem.pollutant_standard == "PM25 Annual 2006"|| elem.pollutant_standard == "PM25 24-hour 2012"|| elem.pollutant_standard == "PM25 Annual 2012"){
+                        return [elem.local_site_name, elem.poc];
+                    }
+                    
+                    });
+                    var anotherst = data.Data.map(function(elem) {
+                        if (elem.pollutant_standard == "Ozone 8-hour 2015" || elem.pollutant_standard == "Ozone 1-hour 1979"){
+                            return [elem.local_site_name, elem.poc];
+                        }
+                        
+                        });
+                     console.log(st)
+                    var ozone1 = data.Data.map(function(elem){
+                    if (elem.pollutant_standard == "Ozone 8-hour 2015"){
+                        return elem.arithmetic_mean;
+                    }
+                    });
+                    var pm = data.Data.map(function(elem){
+                    if (elem.pollutant_standard == "PM25 24-hour 2006"){
+                        return elem.arithmetic_mean;
+                    }
+                    });
+                    var ozone2 = data.Data.map(function(elem){
+                        if (elem.pollutant_standard == "Ozone 1-hour 1979"){
+                            return elem.arithmetic_mean;
+                        }
+                        });
+                    var ozone3 = data.Data.map(function(elem){
+                            if (elem.pollutant_standard == "Ozone 1-hour 1979"){
+                                return elem.arithmetic_mean;
+                         }
+                    });
+                    var pm2 = data.Data.map(function(elem){
+                        if (elem.pollutant_standard == "PM25 Annual 2006"){
+                             return elem.arithmetic_mean;
+                        }
+                    });
+                    var pm3 = data.Data.map(function(elem){
+                        if (elem.pollutant_standard == "PM25 24-hour 2012"){
+                            return elem.arithmetic_mean;
+                        }
+                        });
+                    var pm4 = data.Data.map(function(elem){
+                        if (elem.pollutant_standard == "PM25 Annual 2012"){
+                            return elem.arithmetic_mean;
+                        }
+                    });
+    
+        
+                    
+                    const ctx2 = document.getElementById('canvas2').getContext('2d');
+                    const chart2 = new Chart(ctx2, {
+                    type: 'bar',
+                                data: {
+                                labels: anotherst, 
+                                datasets: [{
+                                    label: ["Ozone 8-hour 2015"],
+                                    data: ozone1,
+                                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                                    borderColor: 'rgb(0, 0, 0)',
+                                    borderWidth: 0.5
+                                },
+                                {
+                                 
+                                    label: ["Ozone 1-hour 1979"],
+                                    data: ozone2,
+                                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                                    borderColor: 'rgb(0, 0, 0)',
+                                    borderWidth: 0.5
+                                },
+                                
+                                ]
+                                },
+                                options: {
+                                    plugins:{
+                                        legend: {
+                                            display: true,
+                                            position: 'bottom',
+                                        
+                                    },
+                                    title:{
+                                        text: "OZONE LEVELS",
+                                        display: true,
+                
+                                        font: {
+                                            weight: 'bold',
+                                            size: 20
+                                        },
+                                    }
+                                }
+                                    
+                                }
+                                
+                            });
+                
+                    
+
+    const ctx = document.getElementById('canvas').getContext('2d');
+    const chart = new Chart(ctx, {
+    type: 'bar',
+                data: {
+                labels: st, 
+                datasets: [{
+                    label: ["PM25 24-hour 2006"],
+                    data: pm,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                    borderColor: 'rgb(0, 0, 0)',
+                    borderWidth: 0.5
+                },
+                {
+                 
+                    label: ["PM25 Annual 2006"],
+                    data: pm2,
+                    backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                    borderColor: 'rgb(0, 0, 0)',
+                    borderWidth: 0.5
+                },
+                {
+                    label: ["PM25 24-hour 2012"],
+                    data: pm3,
+                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                    borderColor: 'rgb(0, 0, 0)',
+                    borderWidth: 0.5
+                },
+                {
+                    label: ["PM25 Annual 2012"],
+                    data: pm4,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    hoverBackgroundColor: "rgba(232,105,90,0.8)",
+                    borderColor: 'rgb(0, 0, 0)',
+                    borderWidth: 0.5
+                }]
+                },
+                options: {
+                    plugins:{
+                        legend: {
+                            display: true,
+                            position: 'bottom',
+                        
+                    },
+                    title:{
+                        text: "PM 2.5 LEVELS",
+                        display: true,
+                       // color: "black",
+                        font: {
+                            weight: 'bold',
+                            size: 20
+                        },
+                    }
+                }
+                    
+                }
+                
+            });
+         }         
+}
+
+};
+
+})}
+)}
